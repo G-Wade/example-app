05be4c558cfe72a219b3c96a645fc557e83e9cf2
G-Wade <gareth.wade@btinternet.com>
2022-10-23 01:08:59 +0100

    Initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<title>My blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My first post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">My second post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post">My third post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet
+        </p>
+    </article>
+</body>
